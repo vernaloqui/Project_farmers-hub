10f4014d7a345be50a2e67da41bcb341a8f97528
vernaloqui <vernadetteloquinario@gmail.com>
2022-10-27 13:55:23 +0800

    added js for the OK button in forgot pw modal

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 //Forgot password modal for OK button
 function forgotPassword(){
     let emailInput = document.getElementById("email").value;
+    let bodyDiv = document.getElementById("forgotPwInstruction");
+    let input = document.getElementById("inputGroup");
+    let footer = document.getElementById("Modalfooter");
+    let noEmail = document.getElementById("inputAlert");
+
+    // footer.style.display = "flex";
+    // bodyDiv.style.display = "block";
+    // bodyDiv.style.display = "block";
 
     if(emailInput !=""){
-        let bodyDiv = document.getElementById("body");
-        let footer = document.getElementById("Modalfooter");
         footer.style.display = "none";
         bodyDiv.style.display = "none";
+        input.style.display = "none";
         
-        document.getElementById("emailSent").innerText = "A password reset link has been sent to your email address.";
+        noEmail.innerText = "A password reset link has been sent to your email address.";
+        location.reload();
     }
     else {
-        let noEmail = document.getElementById("inputAlert");
+       
         noEmail.style.color = "red";
+        noEmail.style.fontSize = "small";
         noEmail.innerText = "Please enter a valid email address.";
     }
 }
+
+//
