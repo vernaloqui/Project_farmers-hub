b8d45c9d5daaa528550afc1b7bc55e1a6d634740
vernaloqui <76933248+vernaloqui@users.noreply.github.com>
2022-10-30 13:03:35 +0800

    newsletter display None once subscribed

--- mockdata.js
+++ mockdata.js
@@ -110,7 +110,8 @@ function confirmGmailSubs(inputText) {
     var format_Suscribe = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
         if(inputText.value.match(format_Suscribe))
         { 
-            window.location.href="OtherIndex.html";
+            //window.location.href="OtherIndex.html";
+            document.querySelector(".Newsletter").style.display = "none";
             document.ValidFormSubs.ValidSubscribe.focus();
         
             return true;
